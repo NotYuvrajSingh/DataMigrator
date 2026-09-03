74a8852e8a03131e7ab950596eff6624edad8f5b
Singh <yuvraj.singh@ecolab.com>
2026-09-03 17:05:31 +0530

    Removed the Autocomplete files from the previous merge

--- Migrator/src/app/component/autocomplete/autocomplete.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-autocomplete',
-  styleUrl: './autocomplete.css',
-  templateUrl: './autocomplete.html',
-})
-export class Autocomplete {}
