c64b73dda15ba7a48b3677bc66b25c81e3aeeab8
Singh <yuvraj.singh@ecolab.com>
2026-09-03 14:45:08 +0530

    Added a branch for Autocomplete in the name search

---
+++ Migrator/src/app/component/autocomplete/autocomplete.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-autocomplete',
+  styleUrl: './autocomplete.css',
+  templateUrl: './autocomplete.html',
+})
+export class Autocomplete {}
