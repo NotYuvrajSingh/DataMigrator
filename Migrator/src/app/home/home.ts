acedf2fee432f33e36d01480b5c33de6c078e86d
Singh <yuvraj.singh@ecolab.com>
2026-09-05 00:57:14 +0530

    AllLocationsSelected custom validator added

--- Migrator/src/app/home/home.ts
+++ Migrator/src/app/home/home.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ReactiveFormsModule, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
-
-// import { MigrationComponent } from './migration-component/migration-component';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators, FormArray, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
-  // imports: [MigrationComponent, ReactiveFormsModule],
   imports: [ReactiveFormsModule],
   selector: 'app-home',
   styleUrl: './home.css',
@@ -46,16 +43,81 @@ export class Home {
   // const http = HttpClient();
   // const fb = FormBuilder();
 
+  atLeastOneSelectedValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const formArray = control as FormArray;
+      const checked = formArray.controls.some(c => c.value);
+      return checked ? null : { required: true };
+    };
+  }
+
+  // What is AbstractControl?
+  // AbstractControl is the base class for FormControl, FormGroup, and FormArray. 
+  // It provides shared behavior and properties for all form controls.
+
+  // What is ValidationErrors?
+  // ValidationErrors is an interface that represents the errors returned by a validator. 
+  // It is a key-value pair where the key is the error name and the value is any additional information about the error.
+
+  // What is ValidatorFn?
+  // ValidatorFn is a function that takes an AbstractControl and returns either a ValidationErrors object or null.
+
+  // What is the .some() method?
+  // The .some() method tests whether at least one element in the array passes the test implemented by the provided function.
+
+  locationValidator(): ValidatorFn {
+    return (control : AbstractControl) => {
+      const formArray = control as FormArray;
+
+      //What is as in JS?
+      // The as keyword is used for type assertion in TypeScript. It tells the compiler to treat a value as a specific type.
+
+      // What is FormArray?
+      // FormArray is a way to manage an array of form controls in Angular. It allows you to dynamically add or remove controls.
+
+      const allLocationsSelected = this.migrationForm?.get('allLocationsSelected')?.value;
+
+      if(allLocationsSelected) {
+        return null;
+      }
+
+      if(formArray.controls.some(c => c.value?.trim()))
+        return null;
+      else
+        return {locationRequired: true};
+
+      // What does some(c => c.value?.trim()) mean?
+      // It checks if at least one control in the FormArray has a non-empty value after trimming whitespace.
+
+      // Whare is locationRequired coming from?
+      // locationRequired is a custom validation error that we are returning if no locations are selected.
+    }
+  }
+
+
   ngOnInit() {
     this.migrationForm = this.fb.group({
       pbiNumber: ['', [Validators.required]],
       sourceAccount: ['', Validators.required],
       destinationAccount: ['', Validators.required],
       allLocationsSelected: [false],
-      locations: this.fb.array([
-        this.fb.control('', Validators.required),
-      ])
-    })
+      locations: this.fb.array(
+        [this.fb.control('')],
+        [this.locationValidator()]
+      ),
+
+      // Why are the fb.control, and this.locationValidator() in two different arrays?
+      // The first array is for the initial form controls, and the second array is for the validators.
+      migrationComponents: this.fb.array(
+        this.migrationComponents.map(() =>
+          this.fb.control(false)),
+        [this.atLeastOneSelectedValidator()]
+      ),
+    });
+
+    this.migrationForm.get('allLocationsSelected')?.valueChanges.subscribe(() => {
+      this.locations.updateValueAndValidity();
+    });
   }
 
   // Why use ngOnInit()
@@ -71,6 +133,9 @@ export class Home {
     // FormArray is a way to manage an array of form controls in Angular.
   }
 
+  get migrationComponentControls() {
+    return this.migrationForm.get('migrationComponents') as FormArray;
+  }
 
   addLocation() {
     // this.locations.push(this.locations.length + 1);
@@ -81,7 +146,7 @@ export class Home {
     // For example, to get the value of the pbiNumber control, you can use this.migrationForm.get('pbiNumber').value. 
 
     this.locations.push(
-      this.fb.control('', Validators.required)
+      this.fb.control('')
     );
   }
 
@@ -107,8 +172,3 @@ export class Home {
     )
   }
 }
-
-// The form can only be submitted if there is atleast one location, even if AllLocationSelected is checked.
-// A turnaround for this could be to add all the locations which are there in the frontend only, when AllLocationsSelected is checked,
-// otherwise, ht locations in the localStorage can bne there.
-// Take other suggestions from manshi.
